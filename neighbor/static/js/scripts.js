547496b3db29a51f71f147e1ac36538497e51f72
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-10-23 00:40:45 +0300

    changes made in the url

--- neighbor/static/js/scripts.js
+++ neighbor/static/js/scripts.js
@@ -164,3 +164,18 @@
             });
 
    })(jQuery);
+
+
+   function getRandomSize(min, max) {
+     return Math.round(Math.random() * (max - min) + min);
+   }
+
+   var allImages = "";
+
+   for (var i = 0; i < 25; i++) {
+     var width = getRandomSize(200, 400);
+     var height =  getRandomSize(200, 400);
+     allImages += '<img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty">';
+   }
+
+   $('#photos').append(allImages);
