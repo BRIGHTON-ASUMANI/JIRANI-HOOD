50e049c9513f23db35d02eda68a6972af4f21744
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-10-22 11:58:09 +0300

    added database config

--- jirani/settings.py
+++ jirani/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'neighbor.apps.NeighborConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'jirani.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighbor',
+        'USER': 'user',
+        'PASSWORD':'newa$um0ney',
     }
 }
 
--- neighbor/models.py
+++ neighbor/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+
+
+class Neighbourhood(models.Model):
+    neighbourhood_photo = models.ImageField(upload_to='neighbourhood/')
+    neighbourhood_name = models.CharField(max_length=100, blank=True, null=True)
+    occupants_count = models.IntegerField(blank=True, null=True)
+    neighbourhood_location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+
+class Business(models.Model):
+    business_photo = models.ImageField(upload_to='business/')
+    business_name = models.CharField(max_length=100, blank=True, null=True)
+    business_description = models.TextField(max_length=200, blank=True, null=True)
+    business_email = models.CharField(max_length=100, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, null=True)
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='profile/')
+    name = models.CharField(max_length=100, blank=True, null=True)
+    status = models.TextField(max_length=140, blank=True, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, null=True)
+    business = models.ForeignKey(Business, on_delete=models.CASCADE, null=True)
