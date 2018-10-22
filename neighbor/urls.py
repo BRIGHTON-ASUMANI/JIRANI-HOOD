80947852212fb720c245073c80930aed08f7d069
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-10-22 12:06:24 +0300

    worked on the user authentication

---
+++ neighbor/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns=[
+    url('^$',views.home,name='home'),
+    url('login/',views.login_user, name='login'),
+    url('logout/',views.logout_user, name='logout'),
+    url('register/',views.register_user, name='register'),
+    url('edit_profile/',views.edit_profile, name='edit_profile'),
+    url('change_password/',views.change_password, name='change_password'),
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
